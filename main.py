94406c8d88d29c89b032988d77de845d530a27f5
github <github>
2024-03-10 15:29:47 +0900

    dd

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
 import pandas
-from japanmap import pref_names
+#from japanmap import pref_names
 import matplotlib.pyplot as plt
 from japanmap import picture
 import streamlit
 
-plt.rcParams['figure.figsize'] = 6, 6
-plt.imshow(picture());
+#plt.rcParams['figure.figsize'] = 6, 6
+#plt.imshow(picture());
+print(type(picture()))
+
