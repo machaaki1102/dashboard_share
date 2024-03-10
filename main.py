3e820fc1e5b18fd4130275f19c434b9c8a296daa
github <github>
2024-03-10 20:16:58 +0900

    ss

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas
-#from japanmap import picture
+from japanmap import picture
 import matplotlib as plt
 import streamlit as st
  
-#s#t.image(picture())
+st.image(picture())
 #print(type(picture()))
 st.write("1")
+
